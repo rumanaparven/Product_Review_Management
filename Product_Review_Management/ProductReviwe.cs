6b15d36ddce41a9bd722e0fd5c693dc829dd3da1
Rumana <rumanaparven16@gmail.com>
2020-11-05 21:13:32 +0530

    [Rumana] Add. Create product review class

---
+++ Product_Review_Management/ProductReviwe.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Product_Review_Management
+{
+    public class ProductReviwe
+    {
+        public int ProductID { get; set; }
+        public int UserID { get; set; }
+        public double Rating { get; set; }
+        public string Review { get; set; }
+        public bool isLike { get; set; }
+    }
+}
--- Product_Review_Management/Program.cs
+++ Product_Review_Management/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Product_Review_Management
 {
@@ -7,6 +8,22 @@ namespace Product_Review_Management
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to product review management");
+     
+            List<ProductReviwe> productReviewList = new List<ProductReviwe>()
+            {
+                new ProductReviwe(){ProductID=1,UserID=1,Rating=5,Review="Good",isLike=true },
+                new ProductReviwe(){ProductID=1,UserID=1,Rating=5,Review="Good",isLike=true },
+                new ProductReviwe(){ProductID=3,UserID=3,Rating=2,Review="Nice",isLike=true },
+                new ProductReviwe(){ProductID=4,UserID=4,Rating=1,Review="Bad",isLike=true },
+                new ProductReviwe(){ProductID=5,UserID=5,Rating=4,Review="Good",isLike=true },
+            };
+            foreach (var list in productReviewList)
+            {
+                Console.WriteLine("ProductID: " + list.ProductID + " UserID: " + list.UserID + " Rating: " + list.Rating + " Review: " + list.Review
+                    + " isLike: " + list.isLike);
+            }
+            
+            
         }
     }
 }
