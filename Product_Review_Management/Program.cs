a0d09a9042fe3a8462463576b4e7f3b70eef7244
Rumana <rumanaparven16@gmail.com>
2020-11-05 20:08:44 +0530

    first commit

---
+++ Product_Review_Management/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Product_Review_Management
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to product review management");
+        }
+    }
+}
